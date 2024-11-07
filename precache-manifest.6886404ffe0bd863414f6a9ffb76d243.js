a3cee770b2f409970d391a684b363d9f3df68a71
DCrivelli <diego.crivelli@mercadolibre.com>
2024-11-07 12:41:24 -0300

    Updates

---
+++ precache-manifest.6886404ffe0bd863414f6a9ffb76d243.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a6b774c842b73bef5f8a96e5afe8aa4",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "57ba419359708ae49de2",
+    "url": "/React/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "985ee8f9eb923514eaa4",
+    "url": "/React/static/css/main.f0a67eb1.chunk.css"
+  },
+  {
+    "revision": "57ba419359708ae49de2",
+    "url": "/React/static/js/2.7d14cb9f.chunk.js"
+  },
+  {
+    "revision": "985ee8f9eb923514eaa4",
+    "url": "/React/static/js/main.58870753.chunk.js"
+  },
+  {
+    "revision": "283b01c4b2c734b504b3",
+    "url": "/React/static/js/runtime~main.d53f88d2.js"
+  },
+  {
+    "revision": "e8f47a72dddf523096a21b4a93f56f89",
+    "url": "/React/static/media/ShadowsIntoLight-Regular.e8f47a72.ttf"
+  },
+  {
+    "revision": "5815a0509ca7bab8929947e9389535a0",
+    "url": "/React/static/media/foto1.5815a050.jpeg"
+  },
+  {
+    "revision": "6403fb008bb8850333f70e026cfd1673",
+    "url": "/React/static/media/foto2.6403fb00.jpeg"
+  },
+  {
+    "revision": "9aa3522ec911d7191c260552c629a403",
+    "url": "/React/static/media/foto3.9aa3522e.jpeg"
+  },
+  {
+    "revision": "99d40f137919cef50c76859b2076c402",
+    "url": "/React/static/media/foto4.99d40f13.jpeg"
+  },
+  {
+    "revision": "836750ce6fa5f12b94d24f912c20c83f",
+    "url": "/React/static/media/img-buzo-azul.836750ce.jpeg"
+  },
+  {
+    "revision": "3e585cbb96f2f4b051957949472da73f",
+    "url": "/React/static/media/img-buzo-homies.3e585cbb.jpeg"
+  },
+  {
+    "revision": "b12dc06177fd65c9d2a9435f2f31964a",
+    "url": "/React/static/media/img-buzo-newworld.b12dc061.jpeg"
+  },
+  {
+    "revision": "a19c4c5c8c79b67b4cdca957ffe03ebe",
+    "url": "/React/static/media/img-buzo-wonder.a19c4c5c.jpeg"
+  },
+  {
+    "revision": "c084654550ef09b0fffcce19398c2c37",
+    "url": "/React/static/media/img-pufer-azul.c0846545.jpeg"
+  },
+  {
+    "revision": "0756197661ec22076a2ceefccb007684",
+    "url": "/React/static/media/img-pufer-marron3.07561976.jpeg"
+  },
+  {
+    "revision": "59a12196b49ca5ffce11a302803c2528",
+    "url": "/React/static/media/img-pufer-roja.59a12196.jpeg"
+  },
+  {
+    "revision": "fc36afc8faa7203654b45bdee9143581",
+    "url": "/React/static/media/img-pufer-roja1.fc36afc8.jpeg"
+  },
+  {
+    "revision": "1f90036d147620a75700266ee6bd1c40",
+    "url": "/React/static/media/img-pufer-roja2.1f90036d.jpeg"
+  },
+  {
+    "revision": "85769a055522a7ba4c2f84f7f8dff62d",
+    "url": "/React/static/media/img-pufer-roja3.85769a05.jpeg"
+  },
+  {
+    "revision": "dceb19420af922f18e600bf141e3e68c",
+    "url": "/React/static/media/img-pufer-roja4.dceb1942.jpeg"
+  },
+  {
+    "revision": "66872460fdde0da66961cb8fe072b2f8",
+    "url": "/React/static/media/img-pufer-violeta1.66872460.jpeg"
+  }
+]);
